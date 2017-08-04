7e5ff2d260d92de45f85439f22f288ba8ea38641
Austin Carter <aucarter@uw.edu>
2017-08-03 20:52:31 -0700

    update to decomp... using draws of all cause to get draws of mort prop... eliminates over 1 problem

--- le_decomp.R
+++ le_decomp.R
@@ -23,16 +23,21 @@ library(data.table); library(ggplot2)
 # } else {
 
 # }
-plot.e0 <- F
+single.cause <- "Congenital birth defects"
+cause.names <- c(single.cause, "All causes")
 year.start <- 1990
 year.end <- 2016
+plot.e0 <- F
+plot.mx <- F
+plot.props <- T
 
 ### Paths
 lt.path <- "/share/gbd/WORK/02_mortality/03_models/5_lifetables/results/lt_loc/with_shock/compiled_summary_lt.csv"
 plot.dir <- paste0(root, "temp/aucarter/le_decomp/")
 dir.create(plot.dir, showWarnings = F)
 e0.plot.path <- paste0(plot.dir, "e0_plots.pdf")
-
+mx.plot.path <- paste0(plot.dir, "mx_plots.pdf")
+prop.plot.path <- paste0(plot.dir, "le_prop_plot.pdf")
 
 ### Functions
 source(paste0(root, "temp/central_comp/libraries/current/r/get_location_metadata.R"))
@@ -62,7 +67,10 @@ prov.list <- loc.table[parent_id == 44533, location_id]
 
 # Life-tables for provinces for each sex in years of analysis
 lt.dt <- fread(lt.path)[location_id %in% prov.list]
-
+# Set last age group n to 5
+lt.dt[age_group_id == max(age_group_id), n := 5]
+# Set radix to 1 instead of 100,000 and adjust and nLx deaths accordingly 
+lt.dt[, c("lx", "dx", "nLx") := .(lx / 100000, dx / 100000, nLx / 100000)]
 # Life-expectancy plots
 if(plot.e0) {
 	pdf(e0.plot.path, width = 11, height = 8.5)
@@ -82,49 +90,66 @@ if(plot.e0) {
 lt.dt <- lt.dt[year %in% c(year.start, year.end)]
 setnames(lt.dt, "year", "year_id")
 
-## Get draws of birth defects and population to make rates
-c.cause <- meta[cause_name == "Congenital birth defects", cause_id]
-cause.dt <- get_draws(gbd_id_field = "cause_id", gbd_id = c.cause, location_ids = prov.list, year_id = c(year.start, year.end),
+## Get draws of birth defects and all cause to make proportions
+c.causes <- sapply(cause.names, function(cause) {
+	meta[cause_name == cause, cause_id]
+})
+cause.dt <- rbindlist(lapply(c.causes, function(cause) {
+	temp.dt <- get_draws(gbd_id_field = "cause_id", gbd_id = cause, location_ids = prov.list, year_id = c(year.start, year.end),
 					  source = "codcorrect", measure_ids = 1)
-pop.dt <- get_population(location_id = prov.list, year_id = c(year.start, year.end), age_group_id = -1, sex_id = -1)
-rate.dt <- merge(cause.dt, pop.dt, by = c("year_id", "sex_id", "age_group_id", "location_id"))
-rate.dt <- rate.dt[, c("year_id", "sex_id", "age_group_id", "location_id", "population", paste0("draw_", 0:999)), with = F]
+}))
+cause.dt[, c("measure_id", "output_version_id") := NULL]
+melt.cause <- melt(cause.dt, id.vars = c("location_id", "year_id", "sex_id", "age_group_id", "cause_id"))
+cast.cause <- dcast(melt.cause, location_id + year_id + sex_id + age_group_id + variable ~ cause_id)
+setnames(cast.cause, paste0(c.causes), names(c.causes))
+cast.cause[, draw := as.integer(gsub("draw_", "", variable))]
+cast.cause[, variable := NULL]
 # Collapse under-1
-rate.dt[age_group_id %in% 2:4, age_group_id := 28]
-rate.dt <- rate.dt[, lapply(.SD, sum), by = c("year_id", "sex_id", "age_group_id", "location_id")]
-# Calculate rates
-rate.dt <- matrix.div(rate.dt, num = paste0("draw_", 0:999), denom ="population")
-rate.dt[, population := NULL]
+cast.cause[age_group_id %in% 2:4, age_group_id := 28]
+cast.cause <- cast.cause[, lapply(.SD, sum), by = c("year_id", "sex_id", "age_group_id", "location_id", "draw")]
+# Calculate prop
+cast.cause[, mx_prop := get(names(c.causes)[1]) / get(names(c.causes)[2])]
+cast.cause[is.na(mx_prop), mx_prop := 0]
+cast.cause[, names(c.causes) := NULL]
 # Fill in missing ages with 0
 ages <- unique(lt.dt$age_group_id)
-fill.ages <- setdiff(ages, unique(rate.dt$age_group_id))
-temp.dt <- rate.dt[age_group_id == 5]
-temp.dt[, (paste0("draw_", 0:999)) := 0]
+fill.ages <- setdiff(ages, unique(cast.cause$age_group_id))
+temp.dt <- cast.cause[age_group_id == 5]
+temp.dt[, mx_prop := 0]
 fill.dt <- rbindlist(lapply(fill.ages, function(age) {
 	out.dt <- copy(temp.dt)
 	out.dt[, age_group_id := age]
 }))
-rate.dt <- rbind(rate.dt, fill.dt)
+cast.cause <- rbind(cast.cause, fill.dt)
 
-# Merge and prep mortality proportions
-merge.dt <- merge(lt.dt, rate.dt, by = c("year_id", "sex_id", "age_group_id", "location_id"))
-merge.dt <- matrix.div(merge.dt, paste0("draw_", 0:999), "mx")
-
-# Draws of mortality proportions long
-melt.dt <- melt(merge.dt, id.vars = c("year_id", "sex_id", "age_group_id", "location_id"), 
-			   measure.vars = paste0("draw_", 0:999), value.name = "mx_prop")
-melt.dt[, draw := as.integer(gsub("draw_", "", variable))]
-melt.dt[, variable := NULL]
-
-# Merge on proportions and calculate cause deleted px
-lt <- merge(lt.dt, melt.dt, by = c("year_id", "sex_id", "age_group_id", "location_id"))
+# Merge with life table
+lt <- merge(lt.dt, cast.cause, by = c("year_id", "sex_id", "age_group_id", "location_id"))
 lt <- merge(lt, age.table[, .(age_group_id, age_group_years_start)], by = "age_group_id")
 setnames(lt, "age_group_years_start", "age")
 lt <- lt[order(ihme_loc_id, year_id, sex_id, draw, age),]
+
+# Plot birth-defect mx
+if(plot.mx) {
+	pdf(mx.plot.path, width = 11, height = 8.5)
+	mx.plot.dt <- merge(lt[year_id == 1990 & sex_id %in% 1:2 & draw == 0, .(year_id, mx_prop, sex_id, ihme_loc_id, age)], 
+						loc.table[, .(ihme_loc_id, location_name)], by = "ihme_loc_id")
+	for(csex in 1:2){
+		mx.plot.sex <- mx.plot.dt[sex_id == csex]
+		sex.name <- ifelse(csex == 1, "Male", "Female")
+		gg <- ggplot(mx.plot.sex) + geom_line(aes(x = age, y = mx_prop, color = location_name)) +
+			  ggtitle(paste0(sex.name, " Provincial Birth Defect Mortality Proportions")) + theme(legend.position = "bottom") +
+			  ylab("Mortality Proportion") + xlab("Age") + guides(color=guide_legend(title="Province"))
+		print(gg)
+	}
+	dev.off()	
+}
+
+## Calculate cause deleted life table
+# px
 lt[, pxdel := px ^ (1 - mx_prop)]
 lt[age == max(age), pxdel := 0]
 
-# Set first age group lx to 100000 and calculate resulting lx
+# Set first age group lx to 1 and calculate resulting lx
 lt[age == 0, lxdel := lx]
 age.list <- sort(unique(lt$age))
 for(i in 2:length(age.list)) {
@@ -142,13 +167,45 @@ lt[, dxdel := lxdel - lxdel_lead]
 lt[age == max(age), dxdel := lxdel]
 
 # update ax
+lt[, axdel := ax]
+ax.ages <- c(0, 1, 5, max(age.list))
+lt[age %in% ax.ages, axdel := n + (1 - mx_prop) * ((1 - px) / (1 - pxdel)) * (ax - n)]
 
 # nLx
+lt[, nLxdel := n * lxdel_lead + axdel * dxdel]
+lt[age == max(age), nLxdel := (ex / (1 - mx_prop)) * lxdel]
 
+# Decompose
+lt[, nLxcause := ifelse(nLxdel == 0, 0, (nLx / nLxdel) * n)]
+cast.lt <- dcast(lt, age + sex_id + location_id + draw + n ~ year_id, value.var = c("nLxcause", "nLxdel"))
+cast.lt[, decomp := (get(paste0("nLxcause_", year.end)) - get(paste0("nLxcause_", year.start))) * ((get(paste0("nLxdel_", year.end)) + get(paste0("nLxdel_", year.start))) / (2 * n)) ]
+collapse.decomp <- cast.lt[, .(total_decomp = sum(decomp)), by = .(sex_id, location_id, draw)]
 
+# LE diff
+diff.dt <- lt.dt[age_group_id == 28 & sex_id %in% 1:2 & year_id %in% c(year.start, year.end), .(year_id, ex, sex_id, location_id)]
+cast.diff <- dcast(diff.dt, sex_id + location_id ~ year_id, value.var = "ex")
+cast.diff[, diff := get(as.character(year.end)) - get(as.character(year.start))]
 
+# Convert to pct of total change in LE
+merge.decomp <- merge(collapse.decomp, cast.diff[, .(sex_id, location_id, diff)], by = c("sex_id", "location_id"))
+merge.decomp[, pct := total_decomp / diff * 100]
 
+# Collapse and summarize
+summary.decomp <- merge.decomp[, .(mean = mean(pct), lower = quantile(pct, 0.025), 
+								  upper = quantile(pct, 0.975)), by = .(sex_id, location_id)]
 
+summary.decomp[, Sex := ifelse(sex_id == 1, "Male", "Female")]
+summary.decomp <- merge(summary.decomp, loc.table[,.(location_id, location_name)], by = "location_id")
 
-
+# Plot proportion of life expectancy improvement from birth defects
+if(plot.props) {
+	pdf(prop.plot.path, width = 11, height = 8.5)	
+	gg <- ggplot(summary.decomp) + geom_bar(aes(x = location_name, y = mean, fill = Sex), position = "dodge", stat = "identity") +
+		  geom_errorbar(aes(x = location_name, ymin = lower, ymax = upper, group = Sex), position = position_dodge(0.9), width = 0.5) +
+		  ggtitle("Proportion of Life Expectancy Gains Due to Improvement in Birth Defects Mortality") + 
+		  theme(legend.position = "bottom") + ylab("Life Expectancy Proportion (Percent)") + xlab("Province") +
+		  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))		  
+		  print(gg)
+	dev.off()		
+}
 ### End
