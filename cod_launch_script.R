80eb5d175529ebfac52c7c637af80f630df0bb14
eeldren <eeldren@uw.edu>
2017-08-08 18:10:51 -0700

    Launch script for cod map code

---
+++ cod_launch_script.R
@@ -0,0 +1,21 @@
+
+library(data.table)
+user <- Sys.info()["user"]
+code.dir <- paste0("/homes/", user, "/chn_practicum_code/")
+n.slots <- 5
+
+cause.list <- c(302,322,339,381,382,383,385,387,642,643,651,652,690,698,706)
+n.jobs <- 0
+for (cause in cause.list) {
+	code.string <- paste0("qsub -pe multi_slot ", n.slots, " ",
+						"-e /share/temp/sgeoutput/", user, "/errors ",
+						"-o /share/temp/sgeoutput/", user, "/output ",
+						"-N prep_data ", 
+						code.dir, "shell_R.sh ", 
+						code.dir, "cod_maps.R ", 
+						cause)
+	print(code.string)
+	system(code.string)
+	n.jobs <- n.jobs + 1
+}
+sprintf("Launched %i jobs", n.jobs)
