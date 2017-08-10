df61ce8b71dc38ee3ed198b6d302e750efa92568
eeldren <eeldren@uw.edu>
2017-08-09 19:57:07 -0700

    Updtaed code to make bar stack order correct

--- cod_stacked_bar.R
+++ cod_stacked_bar.R
@@ -2,7 +2,7 @@
 ## Author: Joe Mikesell (Stata); re-written in R by Erika Eldrenkamp
 ## Date: 04/14/2016; modified 08/09/2017
 
-## SETUP #########################################################
+## SETUP ############################################################
 ## Bring in libraries
 rm(list=ls())
 library(foreign)
@@ -25,6 +25,8 @@ source(paste0(root, "temp/central_comp/libraries/current/r/get_covariate_estimat
 source(paste0(root, "temp/central_comp/libraries/current/r/get_location_metadata.R"))
 source(paste0(root, "temp/central_comp/libraries/current/r/get_outputs.R"))
 
+
+## CODE ##############################################################
 ## Get location metadata
 loc.table <- get_location_metadata(location_set_id = 22)
 
@@ -47,38 +49,37 @@ deaths <- get_outputs(topic="cause", location_id=prov.list, year_id=years, age_g
 lbirths <- get_covariate_estimates(covariate_name_short="live_births_by_sex", location_id=prov.list)
 
 # Generating mortality rate
-lbirths <- subset(lbirths, select=c(location_id, location_name, year_id, mean_value))
+lbirths <- subset(lbirths, select=c(sex_id, location_id, location_name, year_id, mean_value))
 setnames(lbirths, "mean_value", "live_births")
 deaths <- subset(deaths, select=-c(measure_id, metric_id, age_group_id, age_group_name, expected, measure_name, metric_name))
-cod_mr <- merge(lbirths, deaths, by=c("location_id", "year_id", "location_name"), allow.cartesian=TRUE)
+cod_mr <- merge(lbirths, deaths, by=c("location_id", "year_id", "location_name", "sex_id"))
 cod_mr[, rate:=(val/live_births)*100000]
 
 ## Subset to get selected causes
 # Get cause_ids for selected causes
 cause_id_sub <- cod_mr[grepl("encephalopathy|Lower|Congenital birth|Drowning|Neonatal preterm", cod_mr$cause_name), ]
 
-#cause_id_sub <- cod.mr[grepl("encephalopathy|Lower", cod.mr$cause_name), ]
-
 # Subsetting
 cod <- cod_mr[which(cod_mr$cause_id %in% cause_id_sub$cause_id)]
 
-## GRAPHING #########################################################
 
+## GRAPHING #########################################################
 # Generating total mortality rate with which to order provinces
 cod[, mr_sum := sum(rate), by=c("location_id", "year_id", "sex")]
 
-#cod$cause_name <- with(cod, reorder(cause_name, mr_sum))
-#cod$cause_name <- factor(cod$cause_name, levels=cod$val)
-#cod <- cod[year_id==2000 & sex=="Female"]
-#cod <- cod[with(cod, order(desc(mr_sum), desc(val), cause_name))]
-
-# Generating color palette
-# pal15 <- c("#72163b", "#e8995d", "#8752ff", "#7b0810", "#8e4d00", "#cba64d", "#5d9600", "#b6af21",
-# 	"#245f00", "#006739", "#019eea", "#024fd1", "#ff7f9e", "#ed82f0", "#db00a0")
-
 # Set desired sexes
 sexes <- unique(cod$sex)
-#cod$cause_name <- factor(cod$cause_name, levels=cod$cause_name)
+
+# Set order of dataset so bars are in correct order
+index <- c("Lower respiratory infections", "Neonatal preterm birth complications",
+	"Neonatal encephalopathy due to birth asphyxia and trauma", "Congenital birth defects", "Drowning")
+values <- c(1, 2, 3, 4, 5)
+cod$cause_order <- values[match(cod$cause_name, index)]
+
+cod <- cod[with(cod, order(desc(mr_sum), cause_order))]
+cod$cause_name <- factor(cod$cause_name, levels=c("Lower respiratory infections",
+	"Neonatal preterm birth complications", "Neonatal encephalopathy due to birth asphyxia and trauma",
+	"Congenital birth defects", "Drowning"))
 
 # Generate PDF plot
 pdf(paste0(root, "temp/", user, "/mchs/china_cod_sbar.pdf",sep=""),width=12,height=8)
@@ -87,24 +88,18 @@ pdf(paste0(root, "temp/", user, "/mchs/china_cod_sbar.pdf",sep=""),width=12,heig
 for(year in years) {
 	for(sx in sexes) {
 		cod_temp <- cod[year_id==year & sex==sx]
-		cod_temp <- cod_temp[with(cod_temp, order(desc(mr_sum), desc(val), cause_name))]
-		cod_temp$cause_name <- factor(cod_temp$cause_name, levels=cod_temp$cause_name)
 		title=paste("Under-5 mortality by province for", year, ",", sx, sep=" ")
 
 		print(ggplot(data=cod_temp, 
 			aes(reorder(location_name, mr_sum), rate)) +
-			#aes(location_name, rate)) +
 			geom_bar(width=0.85,
 				stat="identity",
 				position=position_fill(),
 				aes(fill=cause_name)) +
 			coord_flip() +
-			#scale_fill_discrete(breaks=rev(levels(cod$val))) +
-			#guides(fill=guide_legend(reverse=TRUE)) +
-			#scale_fill_manual(values=pal15) +
 			labs(title=paste(title, sep=""),
 				y="Mortality rate (per 100,000)",
-				x="Province",
+				x="Province (in order of all-cause mortality rate, per 100,000)",
 				fill="Cause of death"))
 	}
 }
