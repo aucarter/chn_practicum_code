0d2d1a8639807c3eca2fdeb57b16486170c7747b
eeldren <eeldren@uw.edu>
2017-08-10 23:06:47 -0700

    Incl. China mainland and wrapped province names

--- cod_stacked_bar.R
+++ cod_stacked_bar.R
@@ -24,6 +24,7 @@ user <- ifelse(windows, Sys.getenv("USERNAME"), Sys.getenv("USER"))
 source(paste0(root, "temp/central_comp/libraries/current/r/get_covariate_estimates.R"))
 source(paste0(root, "temp/central_comp/libraries/current/r/get_location_metadata.R"))
 source(paste0(root, "temp/central_comp/libraries/current/r/get_outputs.R"))
+source(paste0(root, "temp/central_comp/libraries/current/r/get_population.R"))
 
 
 ## CODE ##############################################################
@@ -32,35 +33,54 @@ loc.table <- get_location_metadata(location_set_id = 22)
 
 ## Generate list of relevant provinces
 prov.list <- setdiff(c(loc.table[parent_id == 6, location_id], loc.table[parent_id == 44533, location_id]), 44533)
-remove <- c(361, 354) # HK and Macao
+remove <- c(361, 354) # HK and Macau
 prov.list <- setdiff(prov.list, remove)
-prov.list[32] <- 6 # China national
 locations <- data.frame(prov.list)
 setnames(locations, "prov.list", "location_id")
 
-## Create mortality rate per 100,000 live births by cause
+## Generate China mainland estimates (w/o HK & Macau) for mortality rates per 100,000 live births
+# Get outputs of MR due to selected causes at province-level
 years <- c(1990, 1995, 2000, 2005, 2010, 2016)
+cause_ids <- c(322, 381, 382, 641, 698)
+mort.dt <- get_outputs(topic="cause", location_id=prov.list, year_id=years, age_group_id=1, sex_id="all",
+	measure_id=1, metric_id=1, cause_id=cause_ids, version="latest")
+cause.table <- subset(mort.dt, cause_id %in% cause_ids)
+cause.table <- subset(cause.table, select=c("cause_id", "cause_name"))
+cause.table <- setkey(cause.table, NULL)
+cause.table <- unique(cause.table)
+loc.table <- subset(loc.table, select=c("location_id", "location_name"))
 
-# Getting under-5 deaths by cause
-deaths <- get_outputs(topic="cause", location_id=prov.list, year_id=years, age_group_id=1, sex_id="all",
-	measure_id=1, metric_id=1, cause_id="lvl3", version="latest")
+# Sum province-level outputs to create China mainland output
+mainland.dt <- copy(mort.dt)
+mainland.dt <- mainland.dt[, lapply(.SD, sum), by = .(year_id, sex_id, sex, cause_id), .SDcols = "val"]
+mainland.dt[, location_id := 44533]
+deaths.dt <- rbind(mort.dt[, .(location_id, year_id, sex_id, sex, cause_id, val)], mainland.dt)
 
-# Getting live births
-lbirths <- get_covariate_estimates(covariate_name_short="live_births_by_sex", location_id=prov.list)
+# Read in live births
+lbirths.dt <- get_covariate_estimates(covariate_name_short="live_births_by_sex", location_id=c(prov.list, 44533))
 
-# Generating mortality rate
-lbirths <- subset(lbirths, select=c(sex_id, location_id, location_name, year_id, mean_value))
-setnames(lbirths, "mean_value", "live_births")
-deaths <- subset(deaths, select=-c(measure_id, metric_id, age_group_id, age_group_name, expected, measure_name, metric_name))
-cod_mr <- merge(lbirths, deaths, by=c("location_id", "year_id", "location_name", "sex_id"))
-cod_mr[, rate:=(val/live_births)*100000]
+# Merge together and calculate rates
+lbirths.dt <- subset(lbirths.dt, select=c(sex_id, location_id, year_id, mean_value))
+setnames(lbirths.dt, "mean_value", "live_births")
+cod.mr <- merge(lbirths.dt, deaths.dt, by=c("location_id", "year_id", "sex_id"))
+cod.mr[, rate:=(val/live_births)*100000]
+cod.mr <- merge(cod.mr, cause.table, by="cause_id")
+cod.mr <- merge(cod.mr, loc.table, by="location_id")
+
+## Merge with SDI estimates
+# Pull SDI at province-level
+sdi <- get_covariate_estimates(covariate_id=881, location_id=c(prov.list, 44533))
+sdi <- subset(sdi, select=c("location_id", "location_name", "year_id", "mean_value"))
+setnames(sdi, old="mean_value", new="sdi")
+
+cod.mr <- merge(cod.mr, sdi, by=c("location_id", "location_name", "year_id"))
 
 ## Subset to get selected causes
 # Get cause_ids for selected causes
-cause_id_sub <- cod_mr[grepl("encephalopathy|Lower|Congenital birth|Drowning|Neonatal preterm", cod_mr$cause_name), ]
+cause_id_sub <- cod.mr[grepl("encephalopathy|Lower|Congenital birth|Drowning|Neonatal preterm", cod.mr$cause_name), ]
 
 # Subsetting
-cod <- cod_mr[which(cod_mr$cause_id %in% cause_id_sub$cause_id)]
+cod <- cod.mr[which(cod.mr$cause_id %in% cause_id_sub$cause_id)]
 
 
 ## GRAPHING #########################################################
@@ -81,8 +101,14 @@ cod$cause_name <- factor(cod$cause_name, levels=c("Lower respiratory infections"
 	"Neonatal preterm birth complications", "Neonatal encephalopathy due to birth asphyxia and trauma",
 	"Congenital birth defects", "Drowning"))
 
-# Generate PDF plot
-pdf(paste0(root, "temp/", user, "/mchs/china_cod_sbar.pdf",sep=""),width=12,height=8)
+# Making mainland China name shorter to fit in figure
+revalue(cod$location_name, c("China (without Hong Kong and Macao)"="China \n without Hong Kong \n and Macao"))
+cod$location_name[cod$location_name == "China (without Hong Kong and Macao)"] <- "China without \n Hong Kong and Macao"
+
+## Generating PDF figure for order by MR and order by SDI
+# Generate figure with provinces ordered by MR
+# Actually plot
+pdf(paste0(root, "temp/", user, "/mchs/china_cod_sbar_mr.pdf",sep=""),width=12,height=8)
 
 # Looping through years and sexes
 for(year in years) {
@@ -98,8 +124,33 @@ for(year in years) {
 				aes(fill=cause_name)) +
 			coord_flip() +
 			labs(title=paste(title, sep=""),
-				y="Mortality rate (per 100,000)",
-				x="Province (in order of all-cause mortality rate, per 100,000)",
+				y="Mortality rate (per 100,000 live births)",
+				x="Province (in order of all-cause mortality rate, high to low)",
+				fill="Cause of death"))
+	}
+}
+dev.off()
+
+# Generate figure with provinces ordered by SDI
+# Actually plot
+pdf(paste0(root, "temp/", user, "/mchs/china_cod_sbar_sdi.pdf",sep=""),width=12,height=8)
+
+# Looping through years and sexes
+for(year in years) {
+	for(sx in sexes) {
+		cod_temp <- cod[year_id==year & sex==sx]
+		title=paste("Under-5 mortality by province for", year, ",", sx, sep=" ")
+
+		print(ggplot(data=cod_temp, 
+			aes(reorder(location_name, desc(sdi)), rate)) +
+			geom_bar(width=0.85,
+				stat="identity",
+				position=position_fill(),
+				aes(fill=cause_name)) +
+			coord_flip() +
+			labs(title=paste(title, sep=""),
+				y="Mortality rate (per 100,000 live births)",
+				x="Province (in order of SDI, low to high)",
 				fill="Cause of death"))
 	}
 }
