9e735fe144e95d7deac716b29b4dad8174e00018
Austin Carter <aucarter@uw.edu>
2017-08-22 00:25:19 -0700

    bd prop to cause fraction

--- birth_defects/bd_prop.R
+++ birth_defects/bd_prop.R
@@ -17,13 +17,8 @@ user <- ifelse(windows, Sys.getenv("USERNAME"), Sys.getenv("USER"))
 library(data.table); library(ggplot2);library(parallel)
 
 ## Arguments
-args <- commandArgs(trailingOnly = TRUE)
-if(length(args) > 0) {
-	loc <- args[1]
-} else {	
-	loc <- "CHN_491"
-}
-cause <- "Congenital birth defects"
+single.cause <- "Congenital birth defects"
+cause.names <- c(single.cause, "All causes")
 years <- c(1996, 2006, 2016)
 ncores <- 10
 
@@ -64,42 +59,46 @@ sex.table <- data.table(sex_id = 1:3, sex = c("Male", "Female", "All"))
 
 ### Code
 prov.list <- loc.table[parent_id == 44533, location_id]
-loc.list <- c(prov.list, 6)
 
 ## Get draws of birth defects to make proportions
-cause.id <- meta[cause_name == cause, cause_id]
-cause.dt <- get_draws(gbd_id_field = "cause_id", gbd_id = cause.id, location_ids = prov.list, year_id = years,
-				  source = "codcorrect", measure_ids = 1, sex_ids = 1:3)
-
+c.causes <- sapply(cause.names, function(cause) {
+	meta[cause_name == cause, cause_id]
+})
+cause.dt <- rbindlist(lapply(c.causes, function(cause) {
+	temp.dt <- get_draws(gbd_id_field = "cause_id", gbd_id = cause, location_ids = prov.list, year_id = years,
+					  source = "codcorrect", measure_ids = 1, sex_id = 3, age_group_id = 22)
+}))
 cause.dt[, c("measure_id", "output_version_id", "metric_id") := NULL]
 melt.cause <- melt(cause.dt, id.vars = c("location_id", "year_id", "sex_id", "age_group_id", "cause_id"))
 cast.cause <- dcast(melt.cause, location_id + year_id + sex_id + age_group_id + variable ~ cause_id)
-setnames(cast.cause, paste0(cause.id), cause)
+setnames(cast.cause, paste0(c.causes), names(c.causes))
 cast.cause[, draw := as.integer(gsub("draw_", "", variable))]
 cast.cause[, variable := NULL]
 
-# Provinces
-bd.dt <- cast.cause[, lapply(.SD, sum, na.rm = T), by = .(location_id, year_id, sex_id, draw), .SDcols = cause]
-
 # Regions
-regions.dt <- merge(bd.dt, regions[, .(location_id, region_name)], by = "location_id")
+regions.dt <- merge(cast.cause, regions[, .(location_id, region_name)], by = "location_id")
 regions.dt[, location_name := region_name]
-regions.dt <- regions.dt[, lapply(.SD, sum, na.rm = T), by = .(location_name, year_id, sex_id, draw), .SDcols = cause]
+regions.dt <- regions.dt[, lapply(.SD, sum, na.rm = T), by = .(location_name, year_id, sex_id, draw), .SDcols = names(c.causes)]
 regions.dt <- merge(regions.dt, regions[, .(location_name, location_id)], by = "location_name")
 regions.dt[, location_name := NULL]
-bound.dt <- rbind(bd.dt, regions.dt, fill = T)
-# Proportions
-total.dt <- bd.dt[, lapply(.SD, sum, na.rm = T), by = .(year_id, sex_id, draw), .SDcols = cause]
-setnames(total.dt, cause, "total")
-merge.bd <- merge(bound.dt, total.dt, by = c("year_id", "sex_id", "draw"))
-merge.bd[, prop := get(cause) / total * 100]
+bound.dt <- rbind(cast.cause, regions.dt, fill = T)
+
+# Mainland
+mainland.dt <- copy(cast.cause)
+mainland.dt[, location_id := 44533]
+mainland.dt <- mainland.dt[, lapply(.SD, sum, na.rm = T), by = .(location_id, year_id, sex_id, draw), .SDcols = names(c.causes)]
+bound.dt <- rbind(bound.dt, mainland.dt, fill = T)
+
+# Rates
+bound.dt[, pct := get(names(c.causes)[1]) / get(names(c.causes)[2]) * 100]
 
 # Summarize and prep for output
-summary.bd <- summarize.draws(merge.bd, id.vars = c("location_id", "year_id", "sex_id"), value.vars = "prop")
-summary.bd <- merge(summary.bd, regions[, .(location_id, location_name, region_name)], by = "location_id")
+summary.bd <- summarize.draws(bound.dt, id.vars = c("location_id", "year_id", "sex_id"), value.vars = "pct")
+summary.bd <- merge(summary.bd, regions[, .(location_id, location_name, region_name)], by = "location_id", all.x = T)
+summary.bd[location_id == 44533, location_name := "China (without Hong Kong and Macao)"]
 setnames(summary.bd, c("year_id"), c("year"))
 summary.bd <- merge(summary.bd, sex.table, by = "sex_id")
-summary.bd <- summary.bd[order(year, sex, -region_name, location_name), .(location_name, region_name, year, sex, prop_mean, prop_lower, prop_upper)]
+summary.bd <- summary.bd[order(year, sex, -region_name, location_name), .(location_name, region_name, year, sex, pct_mean, pct_lower, pct_upper)]
 write.csv(summary.bd, bd.prop.path, row.names = F)
 
 ### End
