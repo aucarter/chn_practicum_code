7753b45ccf7a326bb932f1891a295171ac3eebec
eeldren <eeldren@uw.edu>
2017-08-28 00:51:20 -0700

    Changed cause order

--- cod_launch_script.R
+++ cod_launch_script.R
@@ -4,8 +4,8 @@ user <- Sys.info()["user"]
 code.dir <- paste0("/homes/", user, "/chn_practicum_code/")
 n.slots <- 10
 
-#cause.list <- c(294, 322, 381, 382, 641, 698)
-cause.list <- c(294)
+cause.list <- c(294, 322, 381, 382, 641, 698)
+#cause.list <- c(294)
 n.jobs <- 0
 for (cause in cause.list) {
 	code.string <- paste0("qsub -pe multi_slot ", n.slots, " ",
